6f47b09e8de4ffcb6213c556d6226569f069a679
rebar-sh <rebar.sherwani@yahoo.com>
2020-04-09 23:39:27 +0200

    Html theme init

---
+++ js/main.js
@@ -0,0 +1,94 @@
+$(document).ready(function () {
+    //Start Document Ready Function
+
+    //Menu
+    $('#nav').slicknav();
+
+    // scroll nav
+    $("#header-area").sticky({ topSpacing: 0 });
+
+    // scroll up
+    $.scrollUp();
+
+
+    //Slider
+    $('.bxslider').bxSlider();
+
+
+
+
+
+
+    $('.service-area').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 4
+            }
+        }
+    });
+
+
+
+
+    $('.posts-area').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 3
+            }
+        }
+    });
+
+    $('.testimonial-area').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    });
+
+    $('.client-area').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 4
+            },
+            1000: {
+                items: 6
+            }
+        }
+    });
+
+    //End Docmument Ready Function
+});
